64d00a7ff1f3fa097c59a944785145afeddcc055
zain ul abideen <zk438256@gmail.com>
2025-07-06 04:08:24 +0500

    final

--- server/server.js
+++ server/server.js
@@ -23,13 +23,16 @@ app.use(helmet());
 // CORS configuration
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-  ? [process.env.FRONTEND_URL] 
+  ? [process.env.FRONTEND_URL,'https://zimli-internship.netlify.app'] 
   : ['http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
+if (process.env.NODE_ENV !== 'production') {
+  app.use(morgan('combined'));
+}
 
 // Logging middleware
 app.use(morgan('combined'));
