9c718c9a7fda81bace95acb76a9ef99319b4553b
zain ul abideen <zk438256@gmail.com>
2025-07-06 03:38:31 +0500

    cors solved

--- server/server.js
+++ server/server.js
@@ -22,7 +22,9 @@ app.use(helmet());
 
 // CORS configuration
 app.use(cors({
-  origin:['http://localhost:5173','https://zimli-internship.netlify.app/'],
+  origin: process.env.NODE_ENV === 'production' 
+  ? [process.env.FRONTEND_URL] 
+  : ['http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
