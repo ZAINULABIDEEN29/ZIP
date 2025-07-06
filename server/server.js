566601eb24cc2c6c3f0da7cf214b5238cd11891e
zain ul abideen <zk438256@gmail.com>
2025-07-06 06:50:46 +0500

    fixed

--- server/server.js
+++ server/server.js
@@ -31,16 +31,15 @@ app.use(helmet());
 // CORS configuration
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-  ? [process.env.FRONTEND_URL,'https://zimli.netlify.app/'] 
+  ? [process.env.FRONTEND_URL,'https://zimli.netlify.app'] 
   : ['http://localhost:5173'],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  optionsSuccessStatus: 200 
 }));
 
-if (process.env.NODE_ENV !== 'production') {
-  app.use(morgan('combined'));
-}
+
 
 
 
